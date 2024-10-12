21552bd045e92bd6b6980322960ccef0a971923d
fenrir-sunwentao <sunwentao@fenrir-tec.com>
2024-10-12 15:49:06 +0800

    首页自动滚动到当前高亮link label

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script lang="ts" setup>
 import VMenu from "@/layout/VMenu.vue";
+import useActiveMenu from "@/hooks/useActiveMenu";
+import { onMounted } from "vue";
+
+const activeMenu = useActiveMenu();
+onMounted(() => {
+  activeMenu.moveActiveMenuToCenter();
+});
 </script>
 <template>
   <VMenu />

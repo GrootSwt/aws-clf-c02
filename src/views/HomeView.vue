92ec96987506ddbf7307b87e65f7c02b383ee311
fenrir-sunwentao <sunwentao@fenrir-tec.com>
2024-10-24 08:40:21 +0800

    Add official link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import VMenu from "@/layout/VMenu.vue";
 import useActiveMenu from "@/hooks/useActiveMenu";
 import { onMounted } from "vue";
+import VRouterLink from "@/components/common/VRouterLink.vue";
 
 const activeMenu = useActiveMenu();
 onMounted(() => {
@@ -9,6 +10,20 @@ onMounted(() => {
 });
 </script>
 <template>
-  <VMenu />
+  <section>
+    <article
+      class="flex justify-center items-center py-8 text-blue-400 underline gap-4"
+    >
+      <VRouterLink to="https://www.aws.training/Certification">
+        AWS Certification
+      </VRouterLink>
+      <VRouterLink
+        to="https://d1.awsstatic.com/zh_CN/training-and-certification/docs-cloud-practitioner/AWS-Certified-Cloud-Practitioner_Exam-Guide_C02.pdf"
+      >
+        AWS CLF C02 Exam Guide
+      </VRouterLink>
+    </article>
+    <VMenu />
+  </section>
 </template>
 <style lang="scss" scoped></style>
